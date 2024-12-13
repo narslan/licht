5e46f9966cf5cc836524456dfc2c8cb8a1d60f17
Nevroz Arslan <nevrozarslan@gmail.com>
2024-12-13 08:07:20 +0300

    add tokenizer

--- src/views/tokenizer.ts
+++ src/views/tokenizer.ts
@@ -1,44 +1,19 @@
-const SIMPLE = {
-  '*': 'Any',
-  '|': 'Alt',
-  '(': 'GroupStart',
-  ')': 'GroupEnd'
-}
-const combineOrPush = (soFar, character, location) => {
-  const topIndex = soFar.length - 1
-  if ((soFar.length === 0) || (soFar[topIndex].token !== 'Lit')) {
-    soFar.push({ kind: 'Lit', value: character, loc: location })
-  } else {
-    soFar[topIndex].value += character
-  }
+enum TokenType {
+   Move = 'Move',
+   NullMove = 'NullMove', 
+   EscapeComment = 'EscapeComment', 
+   MoveAnnotation = 'MoveAnnotation',
+   Result = 'Result',
+   Commentary = 'Commentary',
+   TagSymbol = 'TagSymbol',
+   TagString = 'TagString'
 }
 
+export function tokenize (input: string): Token[] {
+  const out: Token[] = []
+  let currentPosition = 0
 
-const tokenize = (text) => {
-  const result = []
-  for (let i = 0; i < text.length; i += 1) {
-    const c = text[i]
-    if (c in SIMPLE) {
-      result.push({ kind: SIMPLE[c], loc: i })
-    } else if (c === '^') {
-      if (i === 0) {
-        result.push({ kind: 'Start', loc: i })
-      } else {
-        combineOrPush(result, c, i)
-      }
-    } else if (c === '$') {
-      if (i === (text.length - 1)) {
-        result.push({ kind: 'End', loc: i })
-      } else {
-        combineOrPush(result, c, i)
-      }
-    } else {
-      combineOrPush(result, c, i)
-    }
-  }
-
-  return result
+  return out
 }
-
 
 export default tokenize
